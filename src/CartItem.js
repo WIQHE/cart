a9fa98765e1e3f16b8e39961438cc5ac8b6ab1cb
WIQHE <skandraturi@Skands-MacBook-Air.local>
2022-10-12 14:53:19 +0530

    added a working increase and decrease quant function for buttons

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,11 +9,24 @@ class CartItem extends React.Component {
             qty:1,
             img:''
         }
-        // this.increaseQuant = this.increaseQuant.bind(this);
+        // this.increaseQuant = this.increaseQuant.bind(this); this is also a method of object binding to method
     }
-    increaseQuant = () => {
-        this.state.qty +=1;
-        console.log(this.state.qty);
+    increaseQuant = () => {//whenever we uses the arrow functions it automatically binds the object to the function in these senarios
+        // this.state.qty +=1; this will cahnge the value but will not re render the react component
+        
+        
+        // this is setState form 1
+        this.setState({
+            qty : this.state.qty + 1
+        });
+    }
+    decreaseQuant = () => {
+        // setState form 2 - if previous state requried use this
+        this.setState((prevState) => {
+            return{
+                qty: prevState.qty -1
+            }
+        });
     }
     render(){
         const {price,title, qty} = this.state;//this is object de structuring
@@ -30,12 +43,14 @@ class CartItem extends React.Component {
                         <img 
                             alt="increase" 
                             className="actions-icons" 
-                            src ="https://cdn-icons-png.flaticon.com/512/1665/1665680.png" onClick={this.increaseQuant}
+                            src ="https://cdn-icons-png.flaticon.com/512/1665/1665680.png" 
+                            onClick={this.increaseQuant}
                         />
                         <img 
                             alt="decrease" 
                             className="actions-icons" 
                             src ="https://cdn-icons-png.flaticon.com/512/1665/1665714.png" 
+                            onClick={this.decreaseQuant}
                         />
                         <img 
                             alt="delete" 
@@ -53,7 +68,7 @@ const styles = {
     image: {
         height: 110,
         width: 110,
-        boderRadius: 4,
+        borderRadius: 4,
         background: '#777'
     }
 }
